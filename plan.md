01291f179e4fa476ab48ffe40bd5cc9c601ad90a
Ikuesiri Onosemuode <ikuesiri.onosemuode@gmail.com>
2023-06-29 19:12:39 +0100

    prisma schema and User Crud Operation set up

--- plan.md
+++ plan.md
@@ -6,7 +6,30 @@
 - Implement CRUD operations
 - Implement Passwordless Authentication
 
-
+<!-- step 2 -->
 ## modules <Operations>
 - Users  < create, Get one, List(get all), Update, delete>
 - tweets < create, Get one, List(get all), delete>
+
+<!-- working with prisma -->
+ - npm i --save-dev prisma  < to install >
+
+ <!-- < to set up schema>  -->
+- npx prisma init --datasource-provider sqlite
+
+ <!-- (1) to perfom migration (after settng up prisma schema) 
+ (1b): it's like a git commit, making changes to the schema structure, will require this to be run again.\
+ (1c) example, the username in the schema was made unique, and this was run again on the terminal as 
+  npx prisma migrate dev --name 'unique username'  -->
+ - npx prisma migrate dev --name 'init' 
+<!-- for running and visualizing the DB layer  -->
+npx prisma studio
+
+<!-- step 3 -->
+<!-- Querying the dataBase and integrating with it -->
+npm install @prisma/client
+<!-- ...and then create an instance of it and use it as an ORM -->
+ - import { PrismaClient } from "@prisma/client";
+ - const prisma = new PrismaClient() 
+ e.g  prisma.<modelname>.findMany() <!-- to find a full list  -->
+
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,31 +1,69 @@
 import { Router } from "express";
-
+import { PrismaClient } from "@prisma/client";
 const router = Router();
+const prisma = new PrismaClient()
 // create user
-router.post('/', (req,res) =>{
-    res.status(501).json({error : 'Not implemented'})
-})
+router.post('/', async(req,res) =>{
+    try {
+       const { name, username, email, bio, isVerified} = req.body;
+       const createdUser =  await prisma.user.create({
+        data: {
+            name,
+            username,
+            email,
+            bio,
+            isVerified
+        }
+       })
+       res.status(201).json(createdUser)
+   } catch (error) {
+      res.status(400).json({error: 'usename and email should be unique'})
+   }
+             
+    
+});
 //get list of users
-router.get('/', (req,res) =>{
-    res.status(501).json({error : 'Not implemented'})
+router.get('/', async(req,res) =>{
+   const allUsers = await prisma.user.findMany();
+    res.json(allUsers)
 })
 
 // get one user
-router.get('/:id', (req,res) =>{
+router.get('/:id', async(req,res) =>{
     const { id } = req.params;
-    res.status(501).json({error : 'Not implemented'+ id})
+    const user = await prisma.user.findUnique({where: {id : Number(id)}})
+    res.status(200).json(user)
 })
 
 // update user
-router.put('/:id', (req,res) =>{
+router.patch('/:id', async(req,res) =>{
     const { id } = req.params;
-    res.status(501).json({error : 'Not implemented'+ id})
+    const body = req.body;
+     try {
+        const updateUser = await prisma.user.update({
+            where: { id: Number(id)},
+            data : body
+        })
+        res.status(201).json(updateUser)
+        
+     } catch (error) {
+         res.status(400).json({error : 'Update failed'})
+        
+     }
 })
 
 ///delete User
-router.delete('/:id', (req,res) =>{
+router.delete('/:id', async(req,res) =>{
     const { id } = req.params;
-    res.status(501).json({error : 'Not implemented'+ id})
+    try {
+        const deleteUser = await prisma.user.delete({
+            where: { id: Number(id)}
+        })
+        res.status(200).json({message: "User successully deleted!"})
+    } catch (error) {
+        res.status(400).json({error : "Unable to delete User"})
+        
+    }
 })
 
 export default router;
